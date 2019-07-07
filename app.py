8f083a37b1892de9f133c795103d766bf92a4b97
Kamtochukwu Okafor <kamto1.okafor@gmail.com>
2019-07-07 14:17:07 -0400

    hello world flask intro

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ import os.path
 from flask import (Flask, flash, redirect, render_template, request,
                    session, url_for, jsonify, send_from_directory, send_file)
 import json
-
-
 app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
