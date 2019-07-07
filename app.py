636e0152848e0a17b72908b7d05fa61054b58d5e
Kamtochukwu Okafor <kamto1.okafor@gmail.com>
2019-07-07 14:10:17 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+    Created on Sun July 7 14:58:57 2018
+    @author: Kam
+"""
+
+import string
+import os
+import os.path
+from flask import (Flask, flash, redirect, render_template, request,
+                   session, url_for, jsonify, send_from_directory, send_file)
+import json
+
+
+app = Flask(__name__)
